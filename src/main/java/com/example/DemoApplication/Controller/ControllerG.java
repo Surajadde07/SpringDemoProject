caa17d242f3eb9c6dd1614bcdd3b289f2871e922
HumbleTyrant <the.tyrant152601@gmail.com>
2026-05-12 15:21:04 +0530

    Controller G

--- src/main/java/com/example/DemoApplication/Controller/ControllerG.java
+++ src/main/java/com/example/DemoApplication/Controller/ControllerG.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class KController {
+public class ControllerG {
     @GetMapping("/hiG")
     public String sayHi() {
         return "Namaste Ji";
